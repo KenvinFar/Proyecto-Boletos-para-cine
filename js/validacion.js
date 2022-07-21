38b21194caf926a3288fb49e08bb0eea3b036882
kevinfar <kef.perez@yavirac.edu.ec>
2022-07-20 21:30:56 -0500

    validacion

--- js/validacion.js
+++ js/validacion.js
@@ -1,2 +1,19 @@
 const usuario = document.getElementById("user")
 const pass = document.getElementById("password")
+const form = document.getElementById("form")
+const parr = document.getElementById("warnings")
+form.addEventListener("submit", e=>{
+    e.preventDefault()
+    let warnings = ""
+    let entrar = false 
+    parr.innerHTML = ""
+    if (usuario.value.length < 3) {
+        warnings += 'Usuario no valido'
+        entrar = true 
+    }
+    if (pass.value.length < 8) {
+        warnings += 'La contraseña debe tener mas de 8 caracteres'
+        entrar = true
+    }
+}
+)
